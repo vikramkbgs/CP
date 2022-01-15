3faf461deed1c463fe1635850eea47393b75202a
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-01-15 16:25:50 +0530

    recursion practice

---
+++ power_four.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+        if (n == 4 || n == 1)
+            return true;
+
+        if (n < 4)
+            return false;
+
+        if (n % 4 == 0)
+            return isPowerOfFour(n / 4);
+        else
+            return false;
+    }
+};
