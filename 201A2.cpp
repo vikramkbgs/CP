67d3b72a5654e36850d514017a01e4d9f233dcb8
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-01-25 16:32:23 +0530

    codeforece

--- 201A2.cpp
+++ 201A2.cpp
@@ -1,22 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    string str; cin>>str;
-    int i, j; i = 0; j = 1;
-    
-    //int flag = 0;
-    
-    while(i < str.size())
+    string str; cin >> str;
+    int a, b; a = b = 0;
+
+    for (int i = 0; i < str.size(); i++)
     {
-        if(str[i] =='W' && str[i+1] == 'U' && str[i+2] == 'B')
+        if (str[i] == 'W' && str[i + 1] == 'U' && str[i + 2] == 'B')
         {
-                str.erase(str.begin()+i, str.begin()+i+3);
-                str.insert(i, " ");
+            a = 1;
+            i += 2;
         }
-          i++;
+        else
+        {
+            if (a && b)   cout << " ";
+            a = 0; b = 1;
+            cout << str[i];
+        }
     }
-    cout<<str;
     return 0;
 }
