3a89bfce111be7394ed2ff8cfbfb7239bcce30f4
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-03 03:31:03 +0530

    codeforces

---
+++ ethab construction 1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 2)
+        cout << -1 << endl;
+    else
+    {
+        if (n % 2 == 0)
+            cout << n << " " << 2 << endl;
+        else
+            cout << n - 1 << " " << 2 << endl;
+    }
+    return 0;
+}
