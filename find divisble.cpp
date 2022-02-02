297e806bfcd81027eaad531cb4c8ee8bc9fb3e0b
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-03 03:41:21 +0530

    codeforces

---
+++ find divisble.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        cout << n << " " << n * 2 << endl;
+    }
+    return 0;
+}
