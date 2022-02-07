ff0dc40b158145be0a7833b7bce8bf8509e23ba3
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-07 22:07:00 +0530

    codeforces

--- Buying Torches.cpp
+++ Buying Torches.cpp
@@ -1,23 +1,23 @@
-#include <bits/stdc++.h>
-#define sz(x) (x).size()
-#define pb push_back
-#define ll long long
+#include<bits/stdc++.h>
+#define sz(x)   (x).size()
+#define  pb     push_back
+#define ll      long long
 using namespace std;
 
 int main()
 {
     int t;
-    cin >> t;
-
-    while (t--)
+    cin>>t;
+    
+    while(t--)
     {
         ll x, y, k;
-        cin >> x >> y >> k;
-
-        ll need = y * k + k;
-        ll n = ((need - x) + (x - 2)) / (x - 1) + 1;
-
-        cout << n + k << endl;
+        cin>>x>>y>>k;
+        
+        ll need = y*k + k;
+        ll n = ((need - x)+(x-2))/(x-1)+1;
+        
+        cout<<n+k<<endl;
     }
     return 0;
 }
---
+++ avoid trygub.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define sz(x) (x).size()
+#define pb push_back
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, hashmap[26] = {0};
+        cin >> n;
+        string s;
+        cin >> s;
+        for (char temp : s)
+        {
+            hashmap[temp - 'a'] += 1;
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            while (hashmap[i] > 0)
+            {
+                cout << (char)('a' + i);
+                hashmap[i]--;
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
