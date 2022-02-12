514099d5a5a1463ae6fdf9dadb0d180bddae0982
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-12 15:49:36 +0530

    codeforces

--- numbers.cc
+++ numbers.cc
@@ -1,26 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll  long long
+#define ll long long
+
+int gcd(int a, int b)
+{
+  if (a == 0)
+    return b;
+  return gcd(b % a, a);
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    ll sum = 0;
-    int i = 1;
-    while(i < n)
+  int n;
+  cin >> n;
+  ll sum = 0;
+  int i = 1;
+  int num = n;
+  int j = n - i;
+  while (i < n && j > 1)
+  {
+    num = n;
+    int basesum = 0;
+    while (num > 0)
     {
-      int basesum = 0;
-      int num = n;
-      int j = n-i;
-      while(num > 0)
-      {
-          basesum += num%j;
-          num -= num%j;
-          num /=j;
-      }
-      i++;
-      sum +=basesum;
+      basesum += num % j;
+      num = num - (num % j);
+      num = num / j;
     }
-    cout<<sum<<endl;
-    return 0;
+    i++;
+    sum += basesum;
+    j = n - i;
+  }
+  int common = gcd(n - 2, sum);
+  cout << sum / common << "/" << (n - 2) / common << endl;
+  return 0;
 }
