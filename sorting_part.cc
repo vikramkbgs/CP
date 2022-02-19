51dca1fb240537d60f6bb424965af3d48fc6225d
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-19 08:09:36 +0530

    codeforces

--- sorting_part.cc
+++ sorting_part.cc
@@ -11,18 +11,32 @@ int main()
     cin >> t;
     while (t--)
     {
-       int n;
-       cin>>n;
-       int arr[n];
-       for(int &i: arr)
-       cin>>i;
+        int n;
+        cin >> n;
+        ll arr[n];
+        ll arr2[n];
+        int flag = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int temp;
+            cin >> temp;
+            arr[i] = temp;
+            arr2[i] = temp;
+        }
+        sort(arr2, arr2 + n);
 
-       sort(arr, arr+(n-1));
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == arr2[i])
+                continue;
+            else
+                flag = 0;
+        }
 
-       if(arr[n-2] > arr[n-1])
-       cout<<"YES"<<endl;
-       else
-       cout<<"NO"<<endl;
+        if (!flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
     }
     return 0;
 }
