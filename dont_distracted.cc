09465acb00e7d4e5908f5458d2b7f614f2db2246
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-26 10:33:30 +0530

    Leetcode

--- dont_distracted.cc
+++ dont_distracted.cc
@@ -56,3 +56,42 @@ int main()
     }
     return 0;
 }
+#include <bits/stdc++.h>
+#define pb push_back
+#define ll long long
+#define sz(x) (x).size()
+using namespace std;
+
+int main()
+{ 
+  // inserting data in multimap
+  int n, s;
+  cin>>s>>n;
+  ll mx = s;
+  int count = 0;
+  multimap<int, int>x;
+  multimap<int,int>::iterator it;
+  
+  for(int i =0; i<n; i++)
+  {
+      int tempx, tempb; // tempx  temprory bonus, tempb temprory bonus
+      cin>>tempx>>tempb;
+      x.insert(pair<int, int>(tempx, tempb));
+  }
+  
+  for (it=x.begin(); it!=x.end(); ++it)
+    {
+        if( (*it).first < mx)
+        {
+            mx += (*it).second;
+            count++;
+        }
+        else
+        break;
+    }
+    if(count == n)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+return 0;  
+}
