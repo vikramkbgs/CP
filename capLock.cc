b38171a260b2ca99c3b9c1753f02dbe88610b9a0
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-02-27 11:37:43 +0530

    Leetcode

---
+++ capLock.cc
@@ -0,0 +1,55 @@
+/*-------------------------------------------------Vikram Roy------------------------------------------------------------------*/
+#include<bits/stdc++.h>
+#define ll long long
+#define input(x)    for(int &(i) : (x))cin>>i   
+
+using namespace std;
+
+int cCap(string s)
+{
+    int c = 0;
+    for(int i=0; i<s.size(); i++)
+    if(s[i] >='A' && s[i] <= 'Z')
+    c++;
+    
+    return c;
+}
+
+bool checkCap(char x)
+{
+    if(x >='A' && x <= 'Z')
+    return true;
+    
+    return false;
+}
+
+
+int main()
+{
+  string s;
+  cin>>s;
+  
+  if(s.size() == cCap(s))
+  {
+      char temp = toupper(s[0]);
+      cout<<temp;
+      for(int i = 1; i<s.size(); i++)
+      {
+          char temp = tolower(s[i]);
+          cout<<temp;
+      }
+  }
+  else if(!checkCap(s[0]) && s.size()-1 == cCap(s))
+  {
+      char temp = toupper(s[0]);
+      cout<<temp;
+      for(int i = 1; i<s.size(); i++)
+      {
+          char temp = tolower(s[i]);
+          cout<<temp;
+      }
+  }
+  else
+  cout<<s<<endl;
+  return 0;
+}
--- countryside.cc
+++ countryside.cc
@@ -8,7 +8,62 @@
 using namespace std;
 
 int rainPick(int arr[], int n)
-{
+{ /*-------------------------------------------------Vikram Roy------------------------------------------------------------------*/
+#include <bits/stdc++.h>
+#define ll long long
+#define input(x)         \
+    for (int &(i) : (x)) \
+    cin >> i
+
+    using namespace std;
+
+    int cCap(string s)
+    {
+        int c = 0;
+        for (int i = 0; i < s.size(); i++)
+            if (s[i] >= 'A' && s[i] <= 'Z')
+                c++;
+
+        return c;
+    }
+
+    bool checkCap(char x)
+    {
+        if (x >= 'A' && x <= 'Z')
+            return true;
+
+        return false;
+    }
+
+    int main()
+    {
+        string s;
+        cin >> s;
+
+        if (s.size() == cCap(s))
+        {
+            char temp = toupper(s[0]);
+            cout << temp;
+            for (int i = 1; i < s.size(); i++)
+            {
+                char temp = tolower(s[i]);
+                cout << temp;
+            }
+        }
+        else if (!checkCap(s[0]) && s.size() - 1 == cCap(s))
+        {
+            char temp = toupper(s[0]);
+            cout << temp;
+            for (int i = 1; i < s.size(); i++)
+            {
+                char temp = tolower(s[i]);
+                cout << temp;
+            }
+        }
+        else
+            cout << s << endl;
+        return 0;
+    }
     int fu, fd, mx1 = 0, c1 = 0, c2 = 0, mx2 = 0; // flag up, flag down, max pick, count
     fu = 0;
     fd = 0;
